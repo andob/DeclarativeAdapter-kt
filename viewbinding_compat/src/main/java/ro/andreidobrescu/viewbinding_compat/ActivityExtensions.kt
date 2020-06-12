d3c6849f744d9b24a1dde658e3eaef2a7985c0da
Andrei Dobrescu <usingvirtualbox@gmail.com>
2020-06-12 15:17:16 +0300

    bugfixes

---
+++ viewbinding_compat/src/main/java/ro/andreidobrescu/viewbinding_compat/ActivityExtensions.kt
@@ -0,0 +1,30 @@
+package ro.andreidobrescu.viewbinding_compat
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+
+internal fun AppCompatActivity.getContentView() =
+    findViewById<ViewGroup>(android.R.id.content).getChildAt(0)
+
+internal fun View.getActivity() : AppCompatActivity
+{
+    var lookupContext=context
+
+    do
+    {
+        if (lookupContext is AppCompatActivity)
+            return lookupContext
+
+        if (lookupContext is android.view.ContextThemeWrapper
+            &&lookupContext.baseContext!=null)
+            lookupContext=lookupContext.baseContext
+
+        else if (lookupContext is androidx.appcompat.view.ContextThemeWrapper
+                &&lookupContext.baseContext!=null)
+            lookupContext=lookupContext.baseContext
+    }
+    while (lookupContext !is AppCompatActivity)
+
+    return lookupContext
+}
--- viewbinding_compat/src/main/java/ro/andreidobrescu/viewbinding_compat/ReflectiveViewBindingFieldSetter.kt
+++ viewbinding_compat/src/main/java/ro/andreidobrescu/viewbinding_compat/ReflectiveViewBindingFieldSetter.kt
@@ -1,73 +1,125 @@
 package ro.andreidobrescu.viewbinding_compat
 
-import android.app.Activity
 import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import ro.andreidobrescu.declarativeadapterkt.view.CellView
+import java.lang.reflect.Field
+import java.lang.reflect.Method
 
+//ButterKnife.bind(activity) -> ReflectiveViewBindingFieldSetter.setup(activity)
+//ButterKnife.bind(view) -> ReflectiveViewBindingFieldSetter.setup(view)
+//ButterKnife.bind(target, activity) -> ReflectiveViewBindingFieldSetter.setup(target, activity)
+//ButterKnife.bind(target, view) -> ReflectiveViewBindingFieldSetter.setup(target, view)
 object ReflectiveViewBindingFieldSetter
 {
-    fun setup(view : View)
-    {
-        if (view is ViewGroup)
-            setup(target = view, view = view.getChildAt(0))
-        else setup(target = view, view = view)
-    }
-
-    private fun Activity.getContentView() =
-        findViewById<ViewGroup>(android.R.id.content).getChildAt(0)
-
-    fun setup(activity : Activity)
+    fun setup(activity : AppCompatActivity)
     {
         setup(target = activity, view = activity.getContentView())
     }
 
-    fun setup(target : Any, activity : Activity)
+    fun setup(view : View)
+    {
+        setup(target = view, view = view)
+    }
+
+    fun setup(target : Any, activity : AppCompatActivity)
     {
         setup(target = target, view = activity.getContentView())
     }
 
-    //setup method cache. target class -> (target, view) -> viewBindingField.set(target, viewBinding)
-    private val setupCache = mutableMapOf<Class<*>, (Any, View) -> (Unit)>()
-
-    //similar to ButterKnife.bind(target, view). Goodbye, BK! :(
     fun setup(target : Any, view : View)
     {
-        if (setupCache.size==1000)
-            setupCache.clear()
+        val viewToBind=
+            if (view is CellView<*>)
+                view.getChildAt(0)
+            else view
 
-        val targetClass=target::class.java
-        if (setupCache.containsKey(targetClass))
+        Implementation.get(target, viewToBind)
+                .setup(target, viewToBind)
+    }
+
+    private class Implementation
+    (
+        private val autoBindings : List<AutoBinding>
+    )
+    {
+        private class AutoBinding
+        (
+            private val viewBindingField : Field,
+            private val viewBindingFactory : Method
+        )
         {
-            val cachedMethod=setupCache[targetClass]!!
-            cachedMethod.invoke(target, view)
-        }
-        else
-        {
-
-            val viewBindingFields=targetClass.getAllFields()
-                .filter { field ->
-                    field.annotations.find { fieldAnnotation ->
-                        fieldAnnotation is AutoViewBinding
-                    }!=null
-                }
-
-            for (viewBindingField in viewBindingFields)
+            fun setup(target : Any, view : View)
             {
-                viewBindingField.isAccessible=true
+                val viewBinding=viewBindingFactory.invoke(null, view)
 
-                val bindingMethod=viewBindingField.type
-                    .declaredMethods.find { method ->
-                        method.name=="bind"&&method.parameterTypes.size==1&&
-                        method.parameterTypes.first()==View::class.java
-                    }!!
+                viewBindingField.set(target, viewBinding)
 
-                setupCache[targetClass]={ target, view ->
-                    val viewBinding=bindingMethod.invoke(null, view)
+                view.getActivity().lifecycle.addObserver(object : LifecycleObserver
+                {
+                    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+                    fun onActivityDestoryed()
+                    {
+                        viewBindingField.set(target, null)
+                    }
+                })
+            }
+        }
 
-                    viewBindingField.set(target, viewBinding)
+        fun setup(target : Any, view : View)
+        {
+            for (autoBinding in autoBindings)
+                autoBinding.setup(target, view)
+        }
+
+        companion object
+        {
+            @JvmStatic
+            private val CACHE_SIZE = 100
+
+            @JvmStatic
+            private val cache = mutableMapOf<Class<*>, Implementation>()
+
+            @JvmStatic
+            fun get(target : Any, view : View) : Implementation
+            {
+                if (cache.size>=CACHE_SIZE)
+                    cache.clear()
+
+                val targetClass=target::class.java
+                if (cache.containsKey(targetClass))
+                    return cache[targetClass]!!
+
+                val autoBindings=mutableListOf<AutoBinding>()
+
+                val viewBindingFields=targetClass.getAllFields()
+                    .filter { field ->
+                        field.annotations.find { fieldAnnotation ->
+                            fieldAnnotation is AutoViewBinding
+                        }!=null
+                    }
+
+                for (viewBindingField in viewBindingFields)
+                {
+                    viewBindingField.isAccessible=true
+
+                    val viewBindingFactory=viewBindingField.type
+                        .declaredMethods.find { method ->
+                            method.name=="bind"&&method.parameterTypes.size==1&&
+                            method.parameterTypes.first()==View::class.java
+                        }!!
+
+                    autoBindings.add(AutoBinding(
+                        viewBindingField = viewBindingField,
+                        viewBindingFactory = viewBindingFactory))
                 }
 
-                setupCache[targetClass]!!.invoke(target, view)
+                val implementation=Implementation(autoBindings)
+                cache[targetClass]=implementation
+                return implementation
             }
         }
     }
